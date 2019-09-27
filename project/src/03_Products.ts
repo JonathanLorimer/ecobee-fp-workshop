99e1b1806bbf535fdf5a6cc81c726efc7b43853c
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-09-27 09:16:45 -0400

    03_Products

--- project/src/03_Products.ts
+++ project/src/03_Products.ts
@@ -8,37 +8,48 @@ export interface Tuple<A, B> {
 type tuple = <A, B>(a: A, b: B) => Tuple<A, B>
 export const tuple: tuple = (a, b) => [a, b]
 
-// Exercise
+// Exercises
 
+// Beginner ===================================
+
+// fst: get the first element out of a tuple
 type fst = <A>(t: Tuple<A, any>) => A
-const fst: fst = (t) => t[0]
+const fst: fst = undefined
 
+// snd: get the second element out of a tuple
 type snd = <B>(t: Tuple<any, B>) => B
-const snd: snd = (t) => t[1]
+const snd: snd = undefined
 
+// Intermediate ===================================
+
+// AssocList is a helper type that is an array of tuples
 type AssocList<A, B> = Array<Tuple<A, B>>
 
+// emptyAssoc: create an empty list of tuples
 type emptyAssoc = <A, B>() => AssocList<A, B>
-const emptyAssoc: emptyAssoc = () => []
+const emptyAssoc: emptyAssoc = undefined
 
+// insertAssoc: append a tuple to an assocList
 type insertAssoc = <A, B>(
   xs: AssocList<A, B>,
   t: Tuple<A, B>,
 ) => AssocList<A, B>
-const insertAssoc: insertAssoc = (xs, t) => [...xs, t]
+const insertAssoc: insertAssoc = undefined
 
-const lookupAssoc = <A, B>(xs: AssocList<A, B>, a: A): Maybe<B> =>
-  xs.reduce<Maybe<B>>(
-    (acc, t) => (a === fst(t) ? just(snd(t)) : acc),
-    nothing(),
-  )
+// lookupAssoc: lookup the second item in the tuple by the first
+// Hint: the tuple might not exist, and you must handle this case
+const lookupAssoc = <A, B>(xs: AssocList<A, B>, a: A): Maybe<B> => undefined
 
+// stringLengthAssoc: take a string and insert a tuple
+// of the string and its length into an assocList
 type stringLengthAssoc = (
   assoc: AssocList<string, number>,
   s: string,
 ) => AssocList<string, number>
-const stringLengthAssoc: stringLengthAssoc = (assoc = emptyAssoc(), s) =>
-  insertAssoc(assoc, [s, s.length])
+const stringLengthAssoc: stringLengthAssoc = (assoc = emptyAssoc(), s) => undefined
 
-const zipAssoc = <A, B>(as: A[], bs: B[]): AssocList<A, B> =>
-  as.reduce((acc, a, i) => insertAssoc(acc, [a, bs[i]]), emptyAssoc<A, B>())
+
+// Advanced ===================================
+
+// zipAssoc: take two normal arrays, and zip them together into an assoc list
+const zipAssoc = <A, B>(as: A[], bs: B[]): AssocList<A, B> => undefined
