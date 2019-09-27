526ed27be0842f64e60cb5f942d57e626dc9fbfd
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-09-27 09:16:19 -0400

    04_linkedList

--- project/src/04_LinkedList.ts
+++ project/src/04_LinkedList.ts
@@ -1,25 +1,19 @@
 export { }
 import { Maybe, just, nothing } from "./02_Maybe"
 
-type Nil = null
-const nil = (): Nil => null
-interface Cons<A> {
-  [0]: A
-  [1]: LinkedList<A>
-}
-type cons = <A>(fst: A, snd: LinkedList<A>) => Cons<A>
-const cons: cons = (fst, snd) => [fst, snd]
+// Implement linked list...
 
-type LinkedList<A> = Cons<A> | Nil
+// Exercise
 
-const nats: LinkedList<number> = cons(1, cons(2, cons(3, cons(4, nil()))))
-const nats2: LinkedList<number> = [1, [2, [3, [4, [5, nil()]]]]]
-
+// head: Dangerously get the first item out of a linked list
 type head = <A>(xs: LinkedList<A>) => A
-const head: head = (xs) => xs[0]
+const head: head = undefined
 
+// safeHead: same as head but handle the error case gracefully
 type safeHead = <A>(xs: LinkedList<A>) => Maybe<A>
-const safeHead: safeHead = (xs) => (xs ? just(xs[0]) : nothing())
+const safeHead: safeHead = undefined
 
+// safeTail: return the end of the linked list, handle the
+// error case gracefully
 type safeTail = <A>(xs: LinkedList<A>) => Maybe<LinkedList<A>>
-const safeTail: safeTail = (xs) => (xs ? just(xs[1]) : nothing())
+const safeTail: safeTail = undefined
