95782a77500111f3ccd60b528a49162fd404d395
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-09-27 09:17:38 -0400

    02_Maybe

--- project/src/02_Maybe.ts
+++ project/src/02_Maybe.ts
@@ -19,24 +19,27 @@ export const just: just = (a) => ({ just: a, type: "just" })
 export type Maybe<A> = Just<A> | Nothing
 
 // Exercises
-const get = <A>(obj: Map<A>, key: string): Maybe<A> => {
-  const val = obj[key]
-  return val ? just<A>(val) : nothing()
-}
-const perform = <A, B>(obj: Map<A>, key: string, fn: (a: A) => B): Maybe<B> => {
-  const val = obj[key]
-  return val ? just<B>(fn(val)) : nothing()
-}
 
+// Beginner ================================
+
+// get: function that takes an object and a key and looksup that
+// key in the object
+// Hint: this maybe returns a value and you must handle the failure case
+const get = <A>(obj: Map<A>, key: string): Maybe<A> => { }
+
+// perform: same as get, but takes a function and applies that function
+// if the key exists in the object
+const perform = <A, B>(obj: Map<A>, key: string, fn: (a: A) => B): Maybe<B> => { }
+
+// pureMaybe: a function that takes a value and puts it in a maybe
+// context
+// Hint: you have already seen a function suspiciously similar to this
 type pureMaybe = <A>(a: A) => Maybe<A>
-const pureMaybe: pureMaybe = just
+const pureMaybe: pureMaybe = undefined
 
+// Intermediate ================================
+
+// mapMaybe: a function that takes a maybe value, and maps a
+// function over it if it exists, otherwise it does nothing
 type mapMaybe = <A, B>(fn: (a: A) => B, m: Maybe<A>) => Maybe<B>
-const mapMaybe: mapMaybe = (f, m) => {
-  switch (m.type) {
-    case "nothing":
-      return m
-    case "just":
-      return just(f(m.just))
-  }
-}
+const mapMaybe: mapMaybe = undefined
