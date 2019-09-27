f02f5eae477ecf7d047bd57987adb694338a90e2
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-09-27 09:14:33 -0400

    06_Compose

--- project/src/06_Compose.ts
+++ project/src/06_Compose.ts
@@ -1,8 +1,24 @@
-const compose = (...fs) => (a) => fs.reduceRight((acc, f) => f(acc), a)
 
-type comp = <A, B, C>(f: (b: B) => C, g: (a: A) => B) => (a: A) => C
-const comp: comp = (f, g) => (a) => f(g(a))
+// Exercise
 
+// Beginner ========================
+
+// comp: create a function that composes two
+// unary functions
+type comp = <A, B, C>(
+  f: (b: B) => C,
+  g: (a: A) => B
+) => (a: A) => C
+const comp: comp = undefined
+
+// Advanced ========================
+
+// compose: create a function that composes an arbitrary
+// number of unary functions
+// Hint: use reduceRight, and don't worry about types
+const compose = undefined
+
+// Examples
 const toUpperCase = (s: string): string => s.toUpperCase()
 const join = (ss: string[]): string => ss.join("")
 comp(comp(parseInt, toUpperCase), join)
